fea3f34e339f4af1a888dd749f3ad26ed4a1730d
Tushal Kumar <133259434+Tushalkumar30@users.noreply.github.com>
2024-03-22 16:11:48 +0530

    1773_CountItemsMatchingNumber.java

--- 1773_CountItemsMatchingNumber.java
+++ 1773_CountItemsMatchingNumber.java
@@ -23,3 +23,22 @@ class Solution {
         return ans;
     }
 }
+// 2nd Solution 
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int ans =0 ;
+        int index =0;
+        if( ruleKey.equals("type")) index =0;
+        else if( ruleKey.equals("color")) index =1;
+        else {
+            index =2;
+        }
+        for( int i =0; i < items.size(); i++){
+            if( items.get(i).get(index).equals(ruleValue)){
+                ans++;
+            }
+        }
+        return ans;
+    }
+    
+}
